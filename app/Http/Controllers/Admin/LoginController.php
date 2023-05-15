2f8e3aa49357543d8ee78e58d4d0f93a6aa74c60
Mohamed Riza <mohamedriza94@gmail.com>
2023-05-15 11:57:41 +0530

    Login Controls

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,8 +4,90 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+    
+    /**
+    * Where to redirect users after login.
+    *
+    * @var string
+    */
+    protected $redirectTo = RouteServiceProvider::ADMIN;
+    
+    /**
+    * Create a new controller instance.
+    *
+    * @return void
+    */
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+    
+    /**
+    * Get the guard to be used during authentication.
+    *
+    * @return \Illuminate\Contracts\Auth\StatefulGuard
+    */
+    protected function guard()
+    {
+        return Auth::guard('admin');
+    }
+    
+    /**
+    * Show the application's login form.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function showLoginForm()
+    {
+        $view_data['title'] = 'Login';
+        return view('admin.login', $view_data);
+    }
+    
+    public function validateLogin(Request $request)
+    {
+        // Attempt to log the user in
+        if ($this->guard()->attempt(['email' => $request->email, 
+        'password' => $request->password])) {
+
+            return redirect()->intended(route('admin.dashboard'));
+        } 
+        
+        // if unsuccessful, then redirect back to the login with the form data
+        return redirect()->back()->withInput($request->only('email'))->withErrors([
+            'password' => 'Invalid Email or Password!'
+        ]);
+    }
+    
+    protected function redirectPath()
+    {
+        return method_exists($this, 'redirectTo')
+        ? $this->redirectTo()
+        : (property_exists($this, 'redirectToRoute')
+        ? redirect()->route($this->redirectToRoute)
+        : redirect()->route('admin.dashboard'));
+    }
+    
+    /**
+    * Log the user out of the application.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+    public function logout(Request $request)
+    {   
+        $this->guard()->logout();
+        Session::flush();
+        $request->session()->regenerate(true);
+
+        return redirect()->route('admin.login');
+    }
 }
